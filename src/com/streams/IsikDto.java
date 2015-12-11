70186e4aa17b19d597b5ea4a1c2d9aed52ad3dfe
oleg.pahhomov <oleg.pahhomov@gmail.com>
2015-12-12 00:09:03 +0200

    stream examples

---
+++ src/com/streams/IsikDto.java
@@ -0,0 +1,12 @@
+package com.streams;
+
+public class IsikDto {
+
+    private Isik isik;
+    private Isik nextIsik;
+
+    public IsikDto(Isik isik, Isik nextIsik) {
+        this.isik = isik;
+        this.nextIsik = nextIsik;
+    }
+}
